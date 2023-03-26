4ce491201c6df5471c42ada662330268949e6774
mostafizmohfw <mostafizmohfw@gmail.com>
2023-03-26 16:48:56 +0600

    some issue fixed

--- resources/views/result/show.blade.php
+++ resources/views/result/show.blade.php
@@ -52,14 +52,7 @@
                                         Correct Answer:
                                         {!! $question->correct_answer !!}
                                     </p>
-                                    <p class="px-5 py-2 text-green-600 flex gap-1"> <svg
-                                            xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
-                                            stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
-                                            <path stroke-linecap="round" stroke-linejoin="round"
-                                                d="M9 12.75L11.25 15 15 9.75M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
-                                        </svg> Correct Answer:
-                                        {!! $question->correct_answer !!}
-                                    </p>
+                                @else
                                     <p
                                         class="px-5 py-2 flex gap-1 @if ($question->user_answered == $question->correct_answer) text-green-600 @else text-red-600 @endif">
                                         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
@@ -68,7 +61,16 @@
                                                 d="M9.75 9.75l4.5 4.5m0-4.5l-4.5 4.5M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
                                         </svg>
                                         You Answered:
-                                        {!! $question->user_answered !!}
+                                        {!! $question->correct_answer !!}
+                                    </p>
+                                    <p class="px-5 py-2 text-green-600 font-bold flex gap-1"> <svg
+                                            xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
+                                            stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+                                            <path stroke-linecap="round" stroke-linejoin="round"
+                                                d="M9 12.75L11.25 15 15 9.75M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
+                                        </svg>
+                                        Correct Answer:
+                                        {!! $question->correct_answer !!}
                                     </p>
                                 @endif
                             </div>
